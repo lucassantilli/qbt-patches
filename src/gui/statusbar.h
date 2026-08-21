73625c8a1691f8818339583fb711cc4f383949fd
lucassantillifuck2fa <ravenholmofficial@gmail.com>
2026-08-21 16:28:35 -0300

    Add files via upload

--- src/gui/statusbar.h
+++ src/gui/statusbar.h
@@ -66,6 +66,8 @@ private:
     void updateFreeDiskSpaceVisibility();
     void updateExternalAddressesLabel();
     void updateExternalAddressesVisibility();
+    void updatePortLabel();
+    void updatePortVisibility();
     void updateSpeedLabels();
 
     QPushButton *m_dlSpeedLbl = nullptr;
@@ -74,6 +76,8 @@ private:
     QWidget *m_freeDiskSpaceSeparator = nullptr;
     QLabel *m_lastExternalIPsLbl = nullptr;
     QWidget *m_lastExternalIPsSeparator = nullptr;
+    QLabel *m_portLbl = nullptr;
+    QWidget *m_portSeparator = nullptr;
     QLabel *m_DHTLbl = nullptr;
     QWidget *m_DHTSeparator = nullptr;
     QPushButton *m_connecStatusLblIcon = nullptr;
