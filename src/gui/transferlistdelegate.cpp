c48d2e0ca52af9370c0902403ad1f644772af08b
Lucas Santilli <47494251+lucassantilli@users.noreply.github.com>
2026-09-14 12:54:22 -0300

    Add files via upload

--- src/gui/transferlistdelegate.cpp
+++ src/gui/transferlistdelegate.cpp
@@ -136,10 +136,8 @@ void TransferListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
             // 3. Apply styling rules based on state
             switch (torrentState)
             {
-			case TorrentState::StalledDownloading:
+
 			case TorrentState::StalledUploading:
-			case TorrentState::Downloading:
-			case TorrentState::ForcedDownloading:
                 {
                     const QColor bgColor = option.palette.color(QPalette::WindowText);
                     painter->fillPath(path, bgColor);
@@ -149,7 +147,6 @@ void TransferListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
 				
 			default:
                 {
-                    // Painted based on the status color, with a white label
                     const QColor bgColor = statusColor.isValid() ? statusColor : option.palette.color(QPalette::Text);
                     painter->fillPath(path, bgColor);
                     painter->setPen(Qt::white);
